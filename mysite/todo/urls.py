18bc861bb66182e7b1342b0fb4d4ecf1cbbab358
Ben Ashworth <bashworth16@yahoo.com>
2022-05-31 02:59:19 -0500

    Finished search by task

--- mysite/todo/urls.py
+++ mysite/todo/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task'),
     path('add', views.add_task, name='add'),
+    path('results', views.search, name='search1'),
     path('remove/<task_id>/', views.remove_task, name='remove'),
+    path('update1/<task_id>/', views.update1, name='update'),
+    path('update1/<task_id>/update_task', views.update_task, name='update_task'),
 ]
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -10,6 +10,20 @@ def task_list(request):
     return render(request, 'todos/index.html', context)
 
 
+def search(request):
+    search_text = request.POST.get('search_text')
+    results1 = ''
+    for jobs in Todo.objects.all():
+        if search_text == jobs.task:
+            results1 = jobs.task
+        else:
+            pass
+    context = {
+        'results': results1
+    }
+    return render(request, 'todos/results.html', context)
+
+
 def add_task(request):
     if request.method == 'POST':
         new_task = Todo()
@@ -23,3 +37,14 @@ def add_task(request):
 def remove_task(request, task_id):
     Todo.objects.filter(id=task_id).delete()
     return render(request, 'todos/remove.html')
+
+
+def update1(request, task_id):
+    return render(request, 'todos/update.html')
+
+
+def update_task(request, task_id):
+    uptask = Todo.objects.get(id=task_id)
+    uptask.task = request.POST.get('text_update')
+    uptask.save()
+    return render(request, 'todos/update_task.html')
